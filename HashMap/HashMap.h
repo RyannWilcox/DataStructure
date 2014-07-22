99966f9159ff34d27bfd0aa6ae1ba0cb72dd6be8
RyannWilcox <ryanwilcox1@yahoo.com>
2014-07-22 13:56:12 -0400

    added #ifndef,#define, #endif

--- HashMap/HashMap.h
+++ HashMap/HashMap.h
@@ -1,29 +1,42 @@
+//
+//  HashMap.h
+//  HashMap
+//
+//  Created by Ryan Wilcox on 7/22/14.
+//  Copyright (c) 2014 Ryan Wilcox. All rights reserved.
+//
+
+#ifndef __HashMap__HashMap__
+#define __HashMap__HashMap__
+
+#include <iostream>
 #include "Node.h"
 
+const int TABLE_SIZE = 128;
+
 class HashMap{
-	private:
-		const int TABLE_SIZE = 128;
-		Node map[TABLE_SIZE];
-		
-	public:
-		
-		HashMap();
-		~HashMap();
-		
-		/*
-		 * returns the value of contained in the 
-		 * node by using the key 
-		 */
-		get(int key);
-		
-		/*
-		 * Puts a value and key into a node in the map
-		 */
-		put(int key,int value);
-		
-		/*
-		 * Creates a hash value for locating the
-		 * position in the hashMap
-		 */
-		hashFunction(int key);		
+private:
+	Node map[TABLE_SIZE];
+	
+public:
+	HashMap();
+	
+	/*
+	 * returns the value of contained in the
+	 * node by using the key
+	 */
+	Node get(int key);
+	
+	/*
+	 * Puts a value and key into a node in the map
+	 */
+	void put(int key,int value);
+	
+	/*
+	 * Creates a hash value for locating the
+	 * position in the hashMap
+	 */
+	int hashFunction(int key);
 };
+
+#endif /* defined(__HashMap__HashMap__) */
--- HashMap/Node.h
+++ HashMap/Node.h
@@ -1,22 +1,36 @@
-class Node{
-	private:
-		int key;
-		int value;
+//
+//  Node.h
+//  HashMap
+//
+//  Created by Ryan Wilcox on 7/22/14.
+//  Copyright (c) 2014 Ryan Wilcox. All rights reserved.
+//
 
-	public:
-		Node();
-		Node(int key,int value);
-		~Node();
-		
-		/* Get the key*/
-		getKey();
-		
-		/* Get value stored in the Node*/
-		getValue();
-		
-		/* Set a new Key in the Node*/
-		setKey(int newKey);
-		
-		/* set new value in the Node*/
-		setValue(int newValue);
+#ifndef __HashMap__Node__
+#define __HashMap__Node__
+
+#include <iostream>
+
+class Node{
+private:
+	int key;
+	int value;
+	
+public:
+	Node();
+	Node(int key,int value);
+	
+	/* Get the key*/
+	int getKey();
+	
+	/* Get value stored in the Node*/
+	int getValue();
+	
+	/* Set a new Key in the Node*/
+	void setKey(int newKey);
+	
+	/* set new value in the Node*/
+	void setValue(int newValue);
 };
+
+#endif /* defined(__HashMap__Node__) */
