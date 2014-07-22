ca826a52db010a393341e1c3674efc4c91b119fa
RyannWilcox <ryanwilcox1@yahoo.com>
2014-07-22 11:06:02 -0400

    HashMap header created
    These functions are for inserting value,key pairs into the HashMap and
    getting values that are already in the HashMap

--- HashMap/HashMap.h
+++ HashMap/HashMap.h
@@ -1,7 +1,24 @@
 #include "Node.h"
 
 class HashMap{
-	public:
-		
 	private:
+		const int TABLE_SIZE = 128;
+		Node map[TABLE_SIZE];
+		
+	public:
+		HashMap();
+		~HashMap();
+		
+		/*
+		 * returns the value of contained in the 
+		 * node by using the key 
+		 */
+		get(int key);
+		
+		/*
+		 * Puts a value and key into a node in the map
+		 */
+		put(int key,int value);
+		
+		
 };
