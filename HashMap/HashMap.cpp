af1bb5fc18467edb0deed2d5f35187ef3135b307
RyannWilcox <ryanwilcox1@yahoo.com>
2014-07-22 13:57:57 -0400

    added a little to the method stubs

--- HashMap/HashMap.cpp
+++ HashMap/HashMap.cpp
@@ -1,16 +1,28 @@
+//
+//  HashMap.cpp
+//  HashMap
+//
+//  Created by Ryan Wilcox on 7/22/14.
+//  Copyright (c) 2014 Ryan Wilcox. All rights reserved.
+//
+
 #include "HashMap.h"
 #include <iostream>
 
 
-HashMap::HashMap(){}
-
-HashMap::~HashMap(){}
+HashMap::HashMap(){
+	std::cout<< "HashMap created" << std::endl;
+}
 
 Node HashMap::get(int key){
+	Node test;
+	return test;
 }
 
 void HashMap::put(int key,int value){
+	std::cout<<"put";
 }
 
-int HashMap::hashFunction(int key){	
+int HashMap::hashFunction(int key){
+	return key;
 }
