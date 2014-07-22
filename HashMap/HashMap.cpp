eb2695da27219f7d0c4e8ce0de893411bdabdaa0
RyannWilcox <ryanwilcox1@yahoo.com>
2014-07-22 11:05:08 -0400

    Method stubs created
    Methods include get(),put(), hashFunction()

--- HashMap/HashMap.cpp
+++ HashMap/HashMap.cpp
@@ -1,1 +1,16 @@
 #include "HashMap.h"
+#include <iostream>
+
+
+HashMap::HashMap(){}
+
+HashMap::~HashMap(){}
+
+Node HashMap::get(int key){
+}
+
+void HashMap::put(int key,int value){
+}
+
+int HashMap::hashFunction(int key){	
+}
