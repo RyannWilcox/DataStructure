f7af0388bd630a809c0bea18a51601412ae09212
RyannWilcox <ryanwilcox1@yahoo.com>
2014-07-31 14:28:05 -0400

    added some code to test the get and put functions

--- HashMap/Main.cpp
+++ HashMap/Main.cpp
@@ -4,7 +4,7 @@
 int main (int argc, char const *argv[]){
 	HashMap test;
 	test.put(3,34);
-	std::cout<<"Object created" << std::endl;
-	std::cout<<"Current code build is successful" << std::endl;
+	int val = test.get(3);
+	std::cout<<"The value retrieved is: " << val << std::endl;
 	return 0;
 }
