590edf0077045f1948f18f5ed78891010c8c4a44
RyannWilcox <ryanwilcox1@yahoo.com>
2014-08-07 14:04:55 -0400

    Main has a loop to control the Hash Map
    This is for example purposes.  You can choose to add to the Map, remove
    from the map and also retrieve values from the map.

--- HashMap/src/Main.cpp
+++ HashMap/src/Main.cpp
@@ -2,28 +2,43 @@
 #include "../header/HashMap.h"
 
 int main (int argc, char const *argv[]){
-	
-	//EXAMPLE OF HASH MAP
-	HashMap test;
-	test.put(3,34);
-	test.put(1,555);
-	test.put(20,323);
-	test.put(55,23);
-	test.put(120,111);
-	int val1 = test.get(3);
-	int val2 = test.get(1);
-	int val3 = test.get(20);
-	int val4 = test.get(55);
-	int val5 = test.get(120);
-	std::cout<<" val1 retrieved is: " << val1 << std::endl;
-	std::cout<<" val2 retrieved is: " << val2 << std::endl;
-	std::cout<<" val3 retrieved is: " << val3 << std::endl;
-	std::cout<<" val4 retrieved is: " << val4 << std::endl;
-	std::cout<<" val5 retrieved is: " << val5 << std::endl;
-	
-	test.remove(3);
-	
-	//This will fail because the key 3 and the value 34 have been removed
-	std::cout<< test.get(3);
+	bool keepLoop = true;
+	int choice;
+	int key;
+	int value;
+	HashMap map;
+   while(keepLoop){
+      std::cout << "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+      std::cout << "\n\t\tHash Map\n\n";
+      std::cout << "1:Put\n2:Get\n3.Remove\n4:Exit";
+      std::cout << "\nEnter your choice(1-4): ";
+      std::cin >> choice;
+       switch(choice){
+           case 1:
+			  		std::cout << "Enter an integer value for a key: \n";
+			  		std::cin >> key;
+			  		std::cout << "Enter an integer value: \n";
+			  		std::cin >> value;
+			  		map.put(key,value);
+					std::cout <<"(" <<key <<", "<< value << ") " << "Added to the Hash Map\n";
+               break;
+           case 2:
+			  		std::cout << "Enter an integer key: \n";
+					std::cin >> key; 
+			  	 	std::cout << "The value found is: " << map.get(key)<<std::endl;
+               break;
+			  case 3:
+			 	 	std::cout << "Enter an integer key: \n";
+			  	 	std::cin >> key;
+			  	 	map.remove(key);
+					break;
+           case 4:
+			  		exit(0);
+               break;
+           default:
+               std::cout<<"Please enter correct choice(1-4)";
+               break;
+       }
+   }
 	return 0;
 }
